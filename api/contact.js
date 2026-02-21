0dbdc5335fb47b9197486b80a382c1474e1a5e61
Aariyan Khan <your_email@gmail.com>
2026-02-21 16:31:38 +0530

    Add contact API

---
+++ api/contact.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ message: "Method Not Allowed" });
+    }
+
+    res.status(200).json({ message: "Contact API working" });
+}
